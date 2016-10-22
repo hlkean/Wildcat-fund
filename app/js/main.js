885656f4b50f3b09555e21818ae7bb989090b40c
hlkean <hlkean@gmail.com>
2016-10-22 16:48:41 -0500

    [homepage] Finished styling

--- app/js/main.js
+++ app/js/main.js
@@ -3,7 +3,18 @@
 
 	$(function(){
 		$('#menu').slicknav();
-		console.log("nav");
 	});
+	$(document).scroll(function () {
+        var y = $(this).scrollTop(); 
+        if (y <= 25) {
+            $('nav, .slicknav_menu').removeClass('fixed');
+            // .css({'height':'75px', 'position':'relative'});
+            // $('.nav-block').height('0');
+        } else {
+            $('nav, .slicknav_menu').addClass('fixed');
+            // css({'height':'50px','position':'fixed'});
+            // $('.nav-block').height('80px');
+        }
+    });
 
 })(jQuery);
